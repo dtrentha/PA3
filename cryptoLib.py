6ccd2c4a5ac38ca0d546a23294da2af6c2c62520
DavidTrentham <David.Trentham@protonmail.com>
2017-11-26 15:08:06 -0500

    first commit

---
+++ cryptoLib.py
@@ -0,0 +1,257 @@
+#!/usr/bin/env python3
+
+import binascii
+import os
+import operator
+import sys
+import argparse
+from Crypto.Cipher import AES
+from multiprocessing import Pool
+
+def hexify(message):
+    return binascii.hexlify(bytearray(message)).decode('utf-8')
+
+def dehexify(hext):
+    text = binascii.unhexlify(hext)
+    return text.decode('utf-8')
+
+#XOR two raw byte strings
+def xorify(block, iv):
+    #result = "%x" % (int(binascii.hexlify(block), 16) ^ int(binascii.hexlify(iv),16))
+    #result = format(result,'0>32')
+    #result = binascii.unhexlify(result.strip())
+    #int_block = int.from_bytes(block, byteorder = 'big', signed = False)
+    #int_iv = int.from_bytes(iv, byteorder= 'big', signed = False)
+    #x = int_block ^ int_iv
+    #return x.to_bytes(len(block), byteorder= 'big', signed = False)
+     
+    #This finally worked!!!!
+    return bytes(map(operator.xor, block, iv))
+
+#Generate an IV in raw bytes
+def genIV():
+    return os.urandom(16) 
+
+#Takes a message of plain text and breaks it into blocks of 128 bits.
+def blockify(message):
+    blocks = []
+    while message:
+        blocks.append(message[:16])
+        message = message[16:]
+    return blocks
+
+#Takes blocks of 128 bits and turns it into a single string.
+def deblockify(blocks):
+    message = bytes('', encoding='utf-8')
+    for i in blocks:
+        message += i
+    return message
+
+#Pads the blocks of the message with int converted into chars.
+def padify(blocks):
+    need = 0
+    padding = 0
+    for i in blocks:
+        if len(i) < 16:
+            tot = 16 - len(i)
+            need = 1
+    if need == 0:
+        pad = chr(16) * 16 
+        blocks.append(pad)
+        return blocks
+    elif need == 1:
+        pad = tot.to_bytes(1, byteorder= 'big', signed = False)  * tot
+        blocks[-1] = blocks[-1] + pad
+        return blocks
+
+#Takes off the padding.
+def depadify(blocks):
+    last = blocks[-1]
+    byte = int(last[-2])
+    if byte == 16:
+        blocks = blocks[:-1]
+        return blocks    
+    else:
+        trim = byte
+        blocks[-1] = last[:-trim]
+        return blocks
+
+
+def encrypt(key, raw):
+    '''
+    Takes in a string of clear text and encrypts it.
+        
+    @param raw: a string of clear text
+    @return: a string of encrypted ciphertext
+    '''
+    if (raw is None) or (len(raw) == 0):
+        raise ValueError('input text cannot be null or empty set')
+    cipher = AES.AESCipher(key[:32], AES.MODE_ECB)
+    ciphertext = cipher.encrypt(raw)
+    return  binascii.hexlify(bytearray(ciphertext)).decode('utf-8')
+    
+def decrypt(key, enc):
+    if (enc is None) or (len(enc) == 0):
+        raise ValueError('input text cannot be null or empty set')
+    enc = binascii.unhexlify(enc)
+    cipher = AES.AESCipher(key[:32], AES.MODE_ECB)
+    enc = cipher.decrypt(enc)
+    return enc
+
+def cbc_enc(message,iv,key):
+    ciblocks = []
+    ciblocks.append(hexify(iv))
+    newiv = iv
+    blocks = blockify(message)
+    blocks = padify(blocks)
+    for i in blocks:
+        ciblock = xorify(i,newiv)
+        ciphertext = encrypt(key,ciblock)
+        ciblocks.append(ciphertext)
+        ciphertext = binascii.unhexlify(ciphertext)
+        newiv = ciphertext
+    return ciblocks
+
+def cbc_dec(ciblocks,key):
+    dec = []
+    ciblocks.reverse()
+    num = len(ciblocks)
+    for i in range(num - 1):
+        hext = ciblocks[0]
+        iv = binascii.unhexlify(ciblocks[1])
+        decblock = decrypt(key, hext)
+        decblock = xorify(decblock,iv)
+        dec.insert(0, decblock)
+        del ciblocks[0] 
+    dec = depadify(dec)
+    message = deblockify(dec)
+    return message
+
+def multi_process(key, ctrs, blocks):
+    for index in range(1,len(ctrs)):
+        ctr = binascii.unhexlify(encrypt(key, ctrs[index]))
+        block = blocks[index]
+        result = xorify(block, ctr)
+        ctrs[index] = hexify(result)
+    return ctrs
+
+def ctr_enc(message, iv, key): 
+    blocks = blockify(message)
+    blocks.insert(0,'0')   #added this to make blocks and ciblocks the same length
+    ciblocks = []
+    
+    ciblocks.append(hexify(iv))
+  
+    for i in range(1,len(blocks)):
+        #iv = binascii.unhexlify(format("%x" % (int(binascii.hexlify(iv), 16) + 1),'0>32'))
+        ivint = int.from_bytes(iv, byteorder = 'big', signed = False) + 1
+        iv = ivint.to_bytes(len(iv), byteorder = 'big', signed = False)
+        ciblocks.append(iv)
+  
+    p = Pool()
+    ciblocks = p.apply(multi_process,args=(key,ciblocks,blocks,))
+    
+    p.close()
+    return ciblocks
+ 
+def ctr_dec(ciblocks, key):
+    iv = binascii.unhexlify(ciblocks[0])
+    blocks = []
+ 
+    blocks.append(iv)
+ 
+    for i in range(1,len(ciblocks)):
+        #iv = binascii.unhexlify(format("%x" % (int(binascii.hexlify(iv), 16) + 1),'0>32'))
+        ivint = int.from_bytes(iv, byteorder = 'big', signed = False) + 1
+        iv = ivint.to_bytes(len(iv), byteorder = 'big', signed = False)
+        blocks.append(iv)
+        ciblocks[i] = binascii.unhexlify(ciblocks[i])
+    
+    
+    p = Pool()
+    blocks = p.apply(multi_process,args=(key,blocks,ciblocks,))
+    p.close() 
+    del blocks[0]
+    
+    for i in range(len(blocks)):
+        blocks[i] = binascii.unhexlify(blocks[i])
+ 
+    message = deblockify(blocks)
+    return message    
+
+def main():
+    blocks = []
+ 
+    parser = argparse.ArgumentParser()
+    parser.add_argument('mode')
+    parser.add_argument('-k')
+    parser.add_argument('-i')
+    parser.add_argument('-o')
+    parser.add_argument('-v', nargs ='?')
+    
+    args = parser.parse_args()
+    
+    mode = args.mode
+  
+    kfile = open(args.k)
+    key = kfile.readline()
+    key = key.rstrip('\n')
+    key = binascii.unhexlify(key)
+
+
+    if args.v != None:
+        ivfile = open(args.v)
+        iv = ivfile.readline()
+        iv = iv.rstrip('\n')
+        iv = binascii.unhexlify(iv)
+    else:
+        iv = genIV()
+
+    
+    if mode == "cbc-enc":
+        ifile = open(args.i, 'rb')
+        output = open(args.o, 'w')
+        message = bytes('', encoding='utf-8')
+        for line in ifile:
+            message += line
+        blocks = cbc_enc(message,iv,key)
+        
+        for i in blocks:
+            output.write("%s\n" % i)
+
+    elif mode == "cbc-dec":
+        ifile = open(args.i, 'r')
+        output = open(args.o, 'wb')
+        blocks = ifile.readlines()
+        for i in range(len(blocks)):
+            blocks[i] = blocks[i].strip('\n')
+        message = cbc_dec(blocks, key)
+        output.write(message)
+
+    elif mode == "ctr-enc":
+        ifile = open(args.i, 'rb')
+        output = open(args.o, 'w') 
+        message = bytes('', encoding='utf-8')
+        for line in ifile:
+            message += line
+        blocks = ctr_enc(message,iv,key)
+     
+        for i in blocks:
+            output.write("%s\n" % i)
+
+    elif mode == "ctr-dec":
+        ifile = open(args.i, 'r')
+        output = open(args.o, 'wb')
+        blocks = ifile.readlines()
+        for i in range(len(blocks)):
+            blocks[i] = blocks[i].strip('\n')
+        message = ctr_dec(blocks,key)
+        output.write(message)
+
+    else:
+        print("Not a valid mode")
+        return
+
+if __name__ == "__main__":
+     main()
+
---
+++ rsaLib.py
@@ -0,0 +1,300 @@
+#!/usr/bin/env python3
+
+import random
+import sys
+import argparse
+import os
+import struct
+from Crypto.Util import number
+import binascii
+
+def factory(n):
+     s = 0
+     m = 0
+     odd = n & 1
+     # n should be an even number
+     while odd == 0:
+         s = s + 1
+         n = n >> 1 # n = n / 2
+         # odd or even checking
+         # for example n = 5 then its binary form is 101
+         # 5 & 1 = 101 & 001 = 001
+         # if n = 6 then its binary form is 110
+         # 110 & 001 = 6 & 1 = 000
+         odd = n & 1
+     m = n
+
+# m should be an odd number now
+     return s, m
+
+def powMod(a, m, n):
+     # trival case
+     if m == 1:
+         return a
+     # a kind of russian peasant algorithm on exponent
+     odd = m & 1
+     # even case
+     if odd == 0:
+         tmp = (a**2) % n
+         tm = int(m // 2)
+         return powMod(tmp, tm, n)
+     # odd case
+     return (a * powMod(a , m - 1, n)) % n
+
+def miler_rabin(n):
+     # trival case
+     if  n < 1:
+         return False
+     if  n < 4:
+         return True
+
+     # Given n, find s so that n-1=2sq for some odd q.
+     s, m = factory(n - 1)
+
+     # n is even when s = 0
+     if s < 1:
+         return False
+
+     # get a random number in range 0 to n
+     # Pick a random a ? {1, ..., n-1}
+     a = random.randrange(2, n - 1)
+
+
+     # If aq=1 then n passes (and exit).
+     b =  powMod(a, m ,n) #pow(a, m)
+
+
+     if b % n == 1:
+         return True
+
+     # For i=0,...,s-1 see if a2iq=-1. If so, n passes (and exit).
+     for k in range(0, s):
+         # b = -1 (mod n)
+         # then b + 1 = 0 (mod n)
+         b_ = b + 1
+         if b_ % n == 0 :
+             return True
+         b = powMod(b,2,n)# b = pow(b, 2) % n
+
+     return False
+
+
+def get_prime(key_length):
+    n = random.getrandbits(key_length)
+
+    while miler_rabin(n) == False:
+        n = random.getrandbits(key_length)
+    return n
+
+def gcd(a,b):
+    while b != 0:
+        a , b = b, a % b
+    #print(a)
+    return a
+
+def multiInverse(e, phi):
+
+    x = 0
+    x1 = 1
+    y = 1
+    y1 = 0
+    phi2 = phi
+    while phi2 != 0:
+        q, r = (e // phi2, e % phi2)
+        e, phi2 = phi2, r
+        x, x1 = x1 - q * x, x
+        y, y1 = y1 - q * y, y
+
+    if x1 < 2:
+        #print(phi + x1)
+        return phi + x1
+    #print(x1)
+    return x1
+
+
+def rsaKeyGen(b):
+
+    p = get_prime(b // 2)
+    q = get_prime(b // 2)
+    while p == q:
+        q = get_prime(b // 2)
+
+    n = p * q
+
+
+    phi = (p - 1) * (q - 1)
+
+    for e in range(3, 1000):
+        g = gcd(e, phi)
+        if g == 1:
+            break
+
+    d = multiInverse(e, phi)
+
+    return(n, e, d)
+
+def goodRandom(r):
+    test = 0
+    while test == 0:
+        test = 1
+        randBits = str(random.getrandbits(r))
+        ran = ''
+        for i in randBits:
+            ran = ran + format(ord(i), 'x')
+        bitBlocks = []
+        check = ran[:]
+        while len(check) > 0:
+            slicelen = min(len(check), 2)
+            bitBlocks.append(check[0:slicelen])
+            check = check[slicelen:]
+        for n in bitBlocks:
+            if n == '30':
+                test = 0
+    #print(binascii.unhexlify(ran))
+    return str(binascii.unhexlify(ran).decode('utf-8'))
+
+
+def rsaPad(message, rbits):
+    m = []
+    for i in message:
+        m.append(format(ord(i), 'x'))
+
+    s = ''
+    for i in m:
+        s = s + str(i)
+
+    pad = "0002"
+    r = goodRandom(rbits)
+    #print(r)
+
+
+    pad += r + "00" + s
+    mess = int(pad)
+
+    return mess
+
+
+def rsaEncrypt(message, e, n, bits):
+
+    m = rsaPad(message, bits // 2)
+    #print(m)
+    return powMod(m, e, n)
+
+def rsaDecrypt(cipher, d, n, bits):
+    #print(cipher)
+    message = powMod(cipher, d, n)
+    #print(message)
+    if (len(str(message)) % 2) != 0:
+        s = '0' + str(message)
+    else:
+        s = str(message)
+    #print(s)
+    mess = []
+    for i,j in zip(s[::2], s[1::2]):
+        h = i + j
+        mess.append(h)
+    for i in range(0, len(mess) -1):
+        if mess[i] == '00':
+            mess = mess[i+1:]
+            break
+    #print(mess)
+    m = ''
+    for i in mess:
+        #print(i)
+        m = m + binascii.unhexlify(i).decode('utf-8')
+
+    return m
+
+
+
+def main():
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('func')
+    parser.add_argument('-k', nargs ='?')
+    parser.add_argument('-i', nargs ='?')
+    parser.add_argument('-o', nargs ='?')
+    parser.add_argument('-p', nargs ='?')
+    parser.add_argument('-s', nargs ='?')
+    parser.add_argument('-n', nargs ='?')
+
+    args = parser.parse_args()
+
+    func = args.func
+
+
+
+    if func == "rsa-enc":
+        if args.k != None:
+            kF = open(args.k, 'r')
+            lines = kF.readlines()
+            kF.close()
+            bits = int(lines[0])
+            n = int(lines[1])
+            e = int(lines[2])
+
+        if args.i != None:
+            iF = open(args.i, 'r')
+            m = iF.readline()
+            m = m.strip()
+            iF.close()
+
+        if args.o != None:
+            oF = open(args.o, 'w')
+
+        oF.write(str(rsaEncrypt(m, e, n, bits)) + '\n')
+        oF.close()
+        return
+
+
+    if func == "rsa-dec":
+        if args.k != None:
+            kF = open(args.k, 'r')
+            lines = kF.readlines()
+            kF.close()
+            bits = int(lines[0])
+            n = int(lines[1])
+            d = int(lines[2])
+
+        if args.i != None:
+            iF = open(args.i, 'r')
+            c = int(iF.readline())
+            iF.close()
+
+        if args.o != None:
+            oF = open(args.o, 'w')
+
+        oF.write(str(rsaDecrypt(c, d, n, bits)) + '\n')
+        oF.close()
+        return
+
+    if func == "rsa-keygen":
+        if args.p != None:
+            pF = open(args.p, 'w')
+
+        if args.s != None:
+            sF = open(args.s, 'w')
+
+        if args.n != None:
+            bits = int(args.n)
+
+        n, e, d = rsaKeyGen(bits)
+
+        pF.write(str(bits) + '\n')
+        pF.write(str(n) + '\n')
+        pF.write(str(e) + '\n')
+        pF.close()
+
+        sF.write(str(bits) + '\n')
+        sF.write(str(n) + '\n')
+        sF.write(str(d) + '\n')
+        sF.close()
+
+
+
+if __name__ == '__main__':
+	main()
+
+
+
+
